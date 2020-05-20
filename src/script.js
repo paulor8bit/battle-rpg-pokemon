22fa5086f781633a678171f83b2075abfc2133e2
paulor8bit <thunder_momon@hotmail.com>
2020-05-20 03:32:35 -0300

    ADD NES CSS

--- src/script.js
+++ src/script.js
@@ -1,24 +1,3 @@
-/////////////////////////////////////////////
-// CONTENTS
-/////////////////////////////////////////////
-// VARIABLES
-// UTILITIES
-  // RANDOM NUMBER GENERATOR
-  // CHARACTER BUILD
-  // ATTACK MULTIPLIER
-  // SFX PLAYER
-  // HP BAR ANIMATION
-// RESET
-// CHARACTER CHOICE
-// HERO ATTACK
-// ENEMY ATTACK
-// ATTACK SEQUENCE
-// MODAL CONTROLS
-
-
-/////////////////////////////////////////////
-// VARIABLES
-/////////////////////////////////////////////
 var music = {},
     typeSprite = '',
     types = [],
@@ -34,7 +13,8 @@ var music = {},
     progressComplete = 0;
 
 function buildVars(){
-  // all the music for the game
+  // toda a música do jogo
+
   // http://downloads.khinsider.com/game-soundtracks/album/pokemon-gameboy-sound-collection
   music = {
     opening: "http://66.90.91.26/ost/pokemon-gameboy-sound-collection/aipycrsoym/101-opening.mp3",
@@ -44,7 +24,7 @@ function buildVars(){
     charmander: "http://66.90.91.26/ost/pokemon-gameboy-sound-collection/wfwdwleyga/149-charmander.mp3",
     squirtle: "http://66.90.91.26/ost/pokemon-gameboy-sound-collection/soagplijvq/152-squirtle.mp3",
     bulbasaur: "http://66.90.91.26/ost/pokemon-gameboy-sound-collection/gvqmhhryki/146-bulbasaur.mp3",
-    machop: "http://66.90.91.26/ost/pokemon-gameboy-sound-collection/viaskmobgb/213-machop.mp3"
+    meowth: "http://66.90.91.26/ost/pokemon-gameboy-sound-collection/viaskmobgb/213-meowth.mp3"
   }
 
 
@@ -54,7 +34,7 @@ function buildVars(){
 
 
 
-  // the data for the game in play
+  // os dados para o jogo em jogo
   gameData = {
     step: 1,
     hero: {},
@@ -63,7 +43,7 @@ function buildVars(){
 
 
 
-  // attack variables
+  // variáveis de ataque
   attackName = '';
   curAttack = {};
   randInt = 0;
@@ -75,7 +55,7 @@ function buildVars(){
 
 
 
-  // available characters
+  // pokemons disponiveis
   characters = [
     {
       name: "pikachu",
@@ -83,9 +63,9 @@ function buildVars(){
       weakness: ['fighting'],
       resistance: ['steel'],
       img: {
-        default: "https://assets.pokemon.com/assets/cms2/img/pokedex/full//025.png",
+        default: "http://www.victoryroad.net/picture.php?s=9792cadf12f01527c0ac0d6b372e8359&albumid=253&pictureid=4034",
         front: "http://rs1263.pbsrc.com/albums/ii631/Pokemon-Vampire-Knight-lover/pikachu_.gif~c200",
-        back: "http://vignette4.wikia.nocookie.net/pokemon/images/5/5b/Pikachu_Back_XY.gif/revision/latest?cb=20141009080948"
+        back: "https://cdn2.scratch.mit.edu/get_image/gallery/4034505_170x100.png"
       },
       hp: {
         current: 500,
@@ -132,9 +112,9 @@ function buildVars(){
       weakness: ['water'],
       resistance: ['grass'],
       img: {
-        default: "https://assets.pokemon.com/assets/cms2/img/pokedex/full//004.png",
-        front: "http://rs772.pbsrc.com/albums/yy9/HybridRainbow88/Charmander.gif~c200",
-        back: "http://vignette1.wikia.nocookie.net/pokemon/images/2/23/Charmander_Back_XY.gif/revision/latest?cb=20141009063457"
+        default: "https://lh3.googleusercontent.com/proxy/mCfc4g7_IrXM9Yr_ajg63KYet_Uv3_stVOmLdZU73g5RwF_Fa93f9kDmUO4UxUCE6VmEVL0sPn86BTdBlhaepUNJ2Wk5XWzP",
+        front: "https://66.media.tumblr.com/tumblr_ma0tijLFPg1rfjowdo1_500.gifv",
+        back: "https://vignette.wikia.nocookie.net/pokemon/images/0/00/Charmander_Back_BW.gif/revision/latest/top-crop/width/220/height/220?cb=20101125033812"
       },
       hp: {
         current: 500,
@@ -181,8 +161,8 @@ function buildVars(){
       weakness: ['electric','grass'],
       resistance: ['normal','fire'],
       img: {
-        default: "https://assets.pokemon.com/assets/cms2/img/pokedex/full//007.png",
-        front: "https://66.media.tumblr.com/ddd22fe10a485ed56a46d958c058a970/tumblr_n9lnpepqkW1scncwdo1_500.gif",
+        default: "https://twibbon.blob.core.windows.net/twibbon/2015/293/3b376826-db5e-4038-9403-277a5c27606d.png",
+        front: "https://66.media.tumblr.com/tumblr_ma4ft6OXxw1rfjowdo1_500.gif",
         back: "http://vignette3.wikia.nocookie.net/pokemon/images/d/d8/Squirtle_XY_Back_Sprite.gif/revision/latest?cb=20141031154426"
       },
       hp: {
@@ -230,9 +210,9 @@ function buildVars(){
       weakness: ['fire'],
       resistance: ['water','psychic'],
       img: {
-        default: "https://assets.pokemon.com/assets/cms2/img/pokedex/full//001.png",
-        front: "https://media.giphy.com/media/iIWW4BM6nNWTu/giphy.gif",
-        back: "http://rs425.pbsrc.com/albums/pp335/Grasaldrea/ShinyBulbasauranimatedback.gif~c200",
+        default: "https://www.pngkey.com/png/full/99-991992_bulbasaur-bulbasaur-pixel.png",
+        front: "https://media.giphy.com/media/HH2bb3Pjq5IB2/200w.gif",
+        back: "https://i.ya-webdesign.com/images/transparent-sprite-bulbasaur-15.gif",
         deranged: "http://rs522.pbsrc.com/albums/w348/Richtoon18/b3617568f13aa750c57eacc45d0b2da7.gif~c200",
         sleep: "https://31.media.tumblr.com/4dd7682db26ac687ef81f0dd06794652/tumblr_msesq5uAIk1r93jsro1_500.gif"
       },
@@ -275,55 +255,55 @@ function buildVars(){
         }
       ]
     },
-    {
-      name: "machop",
-      type: 'fighting',
-      weakness: ['psychic','electric'],
-      resistance: [],
-      img: {
-        default: "https://assets.pokemon.com/assets/cms2/img/pokedex/full//066.png",
-        front: "http://graphics.tppcrpg.net/xy/normal/066F.gif",
-        back:  "http://pokeunlock.com/wp-content/uploads/2015/01/machop-2.gif"
-      },
-      hp: {
-        current: 500,
-        total: 500
-      },
-      attacks: [
-        {
-          name: "low kick",
-          hp: randomNum(40,20),
-          avail: {
-            total: 30,
-            remaining: 30
-          }
-        },
-        {
-          name: "karate chop",
-          hp: randomNum(60,45),
-          avail: {
-            total: 10,
-            remaining: 10
-          }
-        },
-        {
-          name: "seismic toss",
-          hp: randomNum(75,60),
-          avail: {
-            total: 5,
-            remaining: 5
-          }
-        },
-        {
-          name: "hundred furious punches",
-          hp: randomNum(160, 130),
-          avail: {
-            total: 2,
-            remaining: 2
-          }
-        }
-      ]
-    }
+    // {
+    //   name: "meowth",
+    //   type: 'dark',
+    //   weakness: [],
+    //   resistance: [],
+    //   img: {
+    //     default: "https://www.pngitem.com/pimgs/m/165-1654006_pixel-art-pokemon-meowth-hd-png-download.png",
+    //     front: "https://24.media.tumblr.com/4dc1072bdd110a27a9d0922d3a392c8e/tumblr_mrw6pugd3A1rfjowdo1_500.gif",
+    //     back:  "http://pokeunlock.com/wp-content/uploads/2015/01/meowth-2.gif"
+    //   },
+    //   hp: {
+    //     current: 500,
+    //     total: 500
+    //   },
+    //   attacks: [
+    //     {
+    //       name: "bite",
+    //       hp: randomNum(40,20),
+    //       avail: {
+    //         total: 30,
+    //         remaining: 30
+    //       }
+    //     },
+    //     {
+    //       name: "body slam",
+    //       hp: randomNum(60,45),
+    //       avail: {
+    //         total: 10,
+    //         remaining: 10
+    //       }
+    //     },
+    //     {
+    //       name: "foul play",
+    //       hp: randomNum(75,60),
+    //       avail: {
+    //         total: 5,
+    //         remaining: 5
+    //       }
+    //     },
+    //     {
+    //       name: "dark pulse",
+    //       hp: randomNum(160, 130),
+    //       avail: {
+    //         total: 2,
+    //         remaining: 2
+    //       }
+    //     }
+    //   ]
+    // }
   ];
 }
 
@@ -331,45 +311,38 @@ function buildVars(){
 
 
 
-/////////////////////////////////////////////
-// UTILITY
-/////////////////////////////////////////////
-// RANDOM NUMBER GENERATOR
-// min is optional
-function randomNum(max, min){
-  // generate a random number
 
-  // min not required
+// GERADOR DE NÚMEROS ALEATÓRIOS
+// min é opcional
+function randomNum(max, min){
+  // gerar um numero aleatorio
+
+  // min não é requerido
   if(min === undefined || min === '' || min === null){
-    // min default value
+    // valor mininmo
     min = 0;
   }
 
-  // random number, yay
+  // gerar numero radom
   return Math.floor(Math.random() * (max - min) + min);
 }
 
 
 
-// CHARACTER BUILD
-// build the character UI
+// construir a interface do usuário do personagem
 function populateChar(container,character){
   // which img
   var facing = 'front';
   if(character === 'hero'){
-    // we see the back of our hero
-    // a real hero faces danger
+    // back do heroi
     facing = 'back';
   }
 
-  // build the character
   container.append('<section class="char"><img src="'+gameData[character].img[facing]+'" alt="'+gameData[character].name+'"><aside class="data"><h2>'+gameData[character].name+'</h2><div><progress max="'+gameData[character].hp.total+'"></progress><p><span>'+gameData[character].hp.current+'</span>/'+gameData[character].hp.total+'</p></div></aside></section>');
 }
 
 
 
-// ATTACK MULTIPLIER
-// modify attack value for weaknesses & strengths
 function attackMultiplier(attacker, curAttack){
   var defender = 'enemy';
   if(attacker === 'enemy'){
@@ -377,12 +350,12 @@ function attackMultiplier(attacker, curAttack){
   }
 
   if(gameData[defender].weakness.indexOf(gameData[attacker].type) >= 0){
-    // weakness exists
+    // 
     curAttack.hp *= 2;
   }
 
   if(gameData[defender].resistance.indexOf(gameData[attacker].type) >= 0){
-    // weakness exists
+    
     curAttack.hp /= 2;
   }
 
@@ -392,32 +365,32 @@ function attackMultiplier(attacker, curAttack){
 
 
 
-// SFX PLAYER
-// stops music and plays sfx
+
+
 function playSound(name){
-  // load sfx src
+  
   $('audio.sfx').attr('src', music[name])
-  // pause game music
+  
   $('audio.music')[0].pause();
-  // character announce yourself
+  
   $('audio.sfx')[0].play();
 
-  // timeout to stop music at given point
+  
   setTimeout(function(){
-    // pause the sfx
+    
     $('audio.sfx')[0].pause();
-    // start the music again
+    
     $('audio.music')[0].play();
-    // reset the sfx
+    
     $('audio.sfx')[0].currentTime = 0;
   },2000);
 }
 
 
-// HP BAR ANIMATION
-// stop and set health bar
+
+
 function setHP(){
-  // stop health animation and set value
+  
   clearInterval(defendProgressInt);
   clearInterval(progressInt);
   $('.stadium .enemy progress').val(gameData.enemy.hp.current);
@@ -429,33 +402,33 @@ function setHP(){
 
 
 /////////////////////////////////////////////
-// RESET
+
 /////////////////////////////////////////////
 function resetGame(){
-  // set default values for game variables
+  
   buildVars();
 
-  // clear the stadium
+  
   $('.hero').empty();
   $('.enemy').empty();
 
-  // reset
+  
   $('.attack-list li').unbind('click');
   $('.attack-list').empty();
   $('.stadium .enemy').css({'padding':'0'});
-  $('.instructions p').text('Choose your hero');
+  $('.instructions p').text('Escolha seu Pokémon');
 
-  // set & start the opening game music
+  
   $('audio.music').attr('src',music["opening"]);
   $('audio.music')[0].play();
 
-  // empty characters
+  
   $('.characters').empty();
   $('.characters').removeClass('hidden');
 
   for(var i in characters){
-    // build the character list
-    $(".characters").append('<div class="char-container"><img src="'+characters[i].img.default+'" alt="'+characters[i].name+'"><h2>'+characters[i].name+'</h2><span class="type '+characters[i].type+'"></span></div>')
+    
+    $(".characters").append('<div class="char-container"><img src="'+characters[i].img.default+'" alt="'+characters[i].name+'"><h2>'+characters[i].name+'</h2></div>')
   }
   characterChoice();
 }
@@ -467,82 +440,82 @@ $('.logo').click(function(){resetGame();});
 
 
 /////////////////////////////////////////////
-// CHARACTER CHOICE
+
 /////////////////////////////////////////////
 function characterChoice(){
   $('.characters .char-container').click(function(){
-    // you have chosen a character
+    
 
-    // your chosen character name
+    
     var name = $(this).children('h2').text().toLowerCase();
 
     switch(gameData.step){
-      // switch for the current step in the game
+      
 
       case 1:
-        // step 1: choose your hero
+        
         for(var i in characters){
           if(characters[i].name === name){
-            // find and save your chosen hero's data
+            
             gameData.hero = characters[i];
           }
         }
 
-        // remove the character from the available list
+        
         var char = $(this).remove();
-        // build my hero
+        
         populateChar($('.stadium .hero'), 'hero');
 
         for(var i in gameData.hero.attacks){
-          // populate attack list
+          
           $('.attack-list').append('<li><p class="attack-name"><strong>'+gameData.hero.attacks[i].name+'</strong></p><p class="attack-count"><small><span>'+gameData.hero.attacks[i].avail.remaining+'</span>/'+gameData.hero.attacks[i].avail.total+'</small></p></li>');
         }
 
         $('.attack-list').addClass('disabled');
 
-        // update instructions
+        
         $('.instructions p').text('Choose your enemy');
-        // set health bar value
+        
         $('.stadium .hero progress').val(gameData.hero.hp.current);
 
-        // let your hero roar
+        
         playSound(name);
 
-        // move on to choosing an enemy
+        
         gameData.step = 2;
         break;
 
       case 2:
-        // step 2: choose your enemy
+        
         for(var i in characters){
           if(characters[i].name === name){
-            // find and save the enemy data
+            
             gameData.enemy = characters[i];
           }
         }
 
-        // remove the enemy from the list
+        
         var char = $(this).remove();
-        // build the enemy
+        
         populateChar($('.stadium .enemy'), 'enemy');
-        // pad the stadium - give them some breathing room
+        
         $('.stadium .enemy').css({'padding':'25px 0'});
 
-        // update instructions
+        
         $('.instructions p').text('Fight!!!');
 
-        // hide the hero list
+        
         $('.characters').children().slideUp('500', function(){
           $('.characters').addClass('hidden');
         });
 
-        // update enemy health bar value
+        
         $('.stadium .enemy progress').val(gameData.enemy.hp.current);
 
-        // the enemy whimpers in fear
+        
         playSound(name);
 
-        // update step to attack phase and bind click events
+        
         gameData.step = 3;
         attackList();
         break;
@@ -554,25 +527,22 @@ function characterChoice(){
 
 
 
-/////////////////////////////////////////////
-// HERO ATTACK
-/////////////////////////////////////////////
 function attackEnemy(that, callback){
-  // attack the enemy!!!
+  
 
-  // name of your attack
+  
   attackName = that.children('.attack-name').children('strong').text().toLowerCase();
 
   for(var i in gameData.hero.attacks){
     if(gameData.hero.attacks[i].name === attackName){
-      // get chosen attack data
+      
       curAttack = gameData.hero.attacks[i];
     }
   }
 
-  // if there are attacks left
+  
   if(curAttack.avail.remaining > 0){
-    // attack!!!
+    
     $('.attack-list').addClass('disabled');
 
     $('.hero .char img').animate(
@@ -600,58 +570,57 @@ function attackEnemy(that, callback){
       'swing'
     );
 
-    // attack enemy
+    
     gameData.enemy.hp.current -= attackMultiplier('hero', curAttack);
 
     if(gameData.enemy.hp.current <= 0){
-      // Enemy is dead
 
       clearModal();
-    $('.modal-in header').append('<h1>You Enemy is slain</h1><span class="close">x</span>');
-    $('.modal-in section').append('<p>Congratulations! Dare you try again?');
+    $('.modal-in header').append('<h1>Seu adversario perdeu a batalha</h1><span class="close">x</span>');
+    $('.modal-in section').append('<p>Parabéns! Porque não tenta vencer outra batalha?</p> <span class="nes-text is-disabled">Feito com carinho por Paulinho</span>');
     $('.modal-out').slideDown('400');
       modalControls();
 
       gameData.enemy.hp.current = 0;
-      // clear the stadium of the dead
+      
       $('.enemy').empty();
-      // show the available characters
+      
       $('.characters').removeClass('hidden');
       $('.characters').children().slideDown('500');
 
       gameData.enemy = {};
 
-      // choose enemy
+      
       gameData.step = 2;
-      // unbind click for reset
+      
       $('.attack-list li').unbind('click');
     }else{
-      // enemy is still alive (Attack!!!)
+      
 
-      // subtract attack
+      
       curAttack.avail.remaining--;
 
-      // interval to animate health bar
+      
       progressInt = setInterval(function(){
-        // get current value of health bar
+        
         var val = $('.stadium .enemy progress').val();
         val--;
 
-        // update health bar value
+        
         $('.stadium .enemy progress').val(val);
 
         if(val === gameData.enemy.hp.current){
-          // if you've hit your target clear interval
+          
           clearInterval(progressInt);
           progressComplete = 1;
         }
       },1);
 
-      // update health numbers
+      
       $('.stadium .enemy .data p span').text(gameData.enemy.hp.current);
       that.children('.attack-count').children('small').children('span').text(curAttack.avail.remaining);
 
-      // wait a second to recover
+      
       setTimeout(function(){
         // now defend that attack
         defend(that);
@@ -705,8 +674,8 @@ function defend(that){
     // ding dong the hero's dead
 
     clearModal();
-    $('.modal-in header').append('<h1>Your Hero has died</h1><span class="close">x</span>');
-    $('.modal-in section').append('<p>You lose, good day!');
+    $('.modal-in header').append('<h1>Seu Pokémon perdeu</h1><span class="close">x</span>');
+    $('.modal-in section').append('<p>Que pena. Estamos torcendo para você ganhar a próxima</p> <span class="nes-text is-disabled">Feito com carinho por Paulinho</span>');
     $('.modal-out').slideDown('400');
     modalControls()
 
